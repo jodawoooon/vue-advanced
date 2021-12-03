73bbf0db9d43129292f3252d96a596d847f834bb
DAUNJO <jodawoooon@chungbuk.ac.kr>
2021-12-03 17:50:13 +0900

    vue advanced - store 모듈화

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,63 +1,17 @@
 import Vuex from "vuex";
 import Vue from "vue";
-import { fetchNewsList, fetchJobsList, fetchAskList } from "../api/axios";
+import mutations from "./mutations";
+import actions from "./actions.js";
+import getters from "./getters.js";
+import state from "./state.js";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {
-    news: [],
-    jobs: [],
-    ask: [],
-  },
-  getters: {
-    fetchedAsk(state) {
-      return state.ask;
-    },
-  },
-  mutations: {
-    SET_NEWS(state, news) {
-      state.news = news;
-    },
-    SET_JOBS(state, jobs) {
-      state.jobs = jobs;
-    },
-    SET_ASK(state, ask) {
-      state.ask = ask;
-    },
-  },
-  actions: {
-    FETCH_NEWS(context) {
-      fetchNewsList()
-        .then((res) => {
-          console.log(res.data);
-          context.commit("SET_NEWS", res.data);
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-    },
-    FETCH_JOBS(context) {
-      fetchJobsList()
-        .then((res) => {
-          console.log(res.data);
-          context.commit("SET_JOBS", res.data);
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-    },
-    FETCH_ASK(context) {
-      fetchAskList()
-        .then((res) => {
-          console.log(res.data);
-          context.commit("SET_ASK", res.data);
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-    },
-  },
+  state: state,
+  getters: getters,
+  mutations: mutations,
+  actions: actions,
 });
 
 export default store;
