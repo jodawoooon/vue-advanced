85eaa1773a7ced6635921bc8cbf118bfcfca4de6
DAUNJO <jodawoooon@chungbuk.ac.kr>
2021-12-06 16:10:16 +0900

    vue advanced - 디자인패턴

---
+++ slots/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul>
+      <item>아이템1</item>
+      <item>아이템2 <button>click!!!</button></item>
+      <item><img src="./assets/logo.png" width="20" /> 아이템3</item>
+      <item><div style="color: skyblue">아이템4</div></item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from "./components/Item.vue";
+
+export default {
+  components: {
+    Item,
+  },
+  data() {
+    return {
+      items: ["아이템1", "아이템2", "아이템3", "아이템4", "아이템5"],
+    };
+  },
+};
+</script>
+
+<style></style>
